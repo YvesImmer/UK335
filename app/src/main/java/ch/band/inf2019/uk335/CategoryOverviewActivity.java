b64586d9f475bbea590dbafeb4d1ae2cbe94097d
YvesImmer <yvesimmer@gmail.com>
2021-01-29 15:28:55 +0100

    Added Base activities

---
+++ app/src/main/java/ch/band/inf2019/uk335/CategoryOverviewActivity.java
@@ -0,0 +1,14 @@
+package ch.band.inf2019.uk335;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_overview);
+    }
+}
