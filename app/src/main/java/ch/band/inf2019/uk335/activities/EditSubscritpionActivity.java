6ec98cec414921432633bb759bb1470d1aa2f5ba
YvesImmer <yvesimmer@gmail.com>
2021-02-28 13:13:41 +0100

    fixed CHF using stupid whitespace character

--- app/src/main/java/ch/band/inf2019/uk335/activities/EditSubscritpionActivity.java
+++ app/src/main/java/ch/band/inf2019/uk335/activities/EditSubscritpionActivity.java
@@ -269,9 +269,7 @@ public class EditSubscritpionActivity extends AppCompatActivity implements Adapt
                 })
                 .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-
                         dialog.dismiss();
-
                     }
                 })
                 .create();
